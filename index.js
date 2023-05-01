f6418f27806efda70764bec1b5c0ab26d6df6db8
NS-Sheam <123sheamfeni@gmail.com>
2023-05-01 12:46:50 +0600

    server done

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const destinationData = require("./allData/destinationData.json")
 const hotelData = require("./allData/hotelData.json")
 app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Travel guru is running")
 })
-app.get("/hotelData", (req, res) => {
-    res.send(hotelData);
+app.get("/destination", (req, res) => {
+    res.send(destinationData);
+})
+app.get("/destination/:id", (req, res) => {
+    const id = req.params.id;
+    const filterData = hotelData.filter(hd => hd.id === +id);
+    res.send(filterData);
 })
 
 app.listen(port, () => {
